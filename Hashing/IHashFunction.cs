3c0171257d13059854d4fb77e3660f3fd9a601b2
Richard Bohunovsky <risa.bohunovsky@gmail.com>
2025-10-27 11:27:59 +0100

    The Interface for all hash algorithms was created. The first algorithm MurmurHash3 was implemented.

--- Hashing/IHashFunction.cs
+++ Hashing/IHashFunction.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace BloomFilter.Hashing
 {
-    internal class IHashFunction
+    /// <summary>
+    /// Defines an interface for a hash function used by the Bloom filter.
+    /// </summary>
+    public interface IHashFunction
     {
+        /// <summary>
+        /// Computes the hash of the given data.
+        /// </summary>
+        /// <param name="data">The byte array to hash.</param>
+        /// <returns>A 32-bit unsigned integer hash code.</returns>
+        uint Hash(byte[] data);
     }
 }
